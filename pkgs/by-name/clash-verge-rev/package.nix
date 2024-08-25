d4da0427eb4cffcd141fd4b80927f3572635a385
zendo <linzway@qq.com>
2024-08-25 09:20:16 +0800

    update

--- pkgs/by-name/clash-verge-rev/package.nix
+++ pkgs/by-name/clash-verge-rev/package.nix
@@ -13,11 +13,11 @@
 
 stdenv.mkDerivation rec {
   pname = "clash-verge-rev";
-  version = "1.7.6";
+  version = "1.7.7";
 
   src = fetchurl {
     url = "https://github.com/clash-verge-rev/clash-verge-rev/releases/download/v${version}/clash-verge_${version}_amd64.deb";
-    hash = "sha256-1hxad8VpXJVHQcksrGwT+gezDhNFglKwoNQh9xF+vVU=";
+    hash = "sha256-Mx0b9c8nF8alxmGs7lBFh3XlcnbVvjIsLoSCyFFcHFo=";
   };
 
   nativeBuildInputs = [
