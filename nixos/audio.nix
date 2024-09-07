fe734f89e197529c01db16009f35640e3f05d484
zendo <linzway@qq.com>
2024-09-07 22:35:27 +0800

    pipewire: use by dafault

--- nixos/audio.nix
+++ nixos/audio.nix
@@ -1,17 +1,11 @@
-{ lib, config, ... }:
+{ config, ... }:
 {
-  # Using PipeWire as the sound server conflicts with PulseAudio
-  hardware.pulseaudio.enable = lib.mkForce false;
-
   # This allows PipeWire to run with realtime privileges (i.e: less cracks)
   security.rtkit.enable = config.services.pipewire.enable;
 
   # PipeWire is a relatively new low-level multimedia framework.
   services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
+    # alsa.support32Bit = true;
     # jack.enable = true;
   };
 }
