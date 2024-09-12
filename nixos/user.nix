bc099d7a44d658faa2e0fe58332611d6f1956a42
zendo <linzway@qq.com>
2024-09-12 23:22:27 +0800

    users: remove systemuser setup

--- nixos/user.nix
+++ nixos/user.nix
@@ -20,23 +20,15 @@ in
 {
   users = {
     mutableUsers = false;
-    # defaultUserShell = pkgs.zsh;
-
-    # groups.${username} = { };
 
     users.${username} = {
       isNormalUser = true;
-      # isSystemUser = true;
       # `mkpasswd`
       initialHashedPassword = "$y$j9T$aNhZV153pAbvGMeFqjGmn.$iH18jxovF5Huof8U4NNPK/EVWHH75o5x8lRzq8IHZO3";
       # ssh-copy-id -i ~/.ssh/id_ed25519.pub nixos@192.168.122.85
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMLrQVhdLD9o1Iq17LKFNQ21PaHIAylizOFkvh74FUrz linzway@qq.com"
       ];
-      # createHome = true;
-      # home = "/home/${username}";
-      # group = username;
-      # uid = 1000;
       extraGroups = [
         "wheel"
         "networkmanager"
