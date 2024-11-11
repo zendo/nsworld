9e173c2be5af4eeb589b31baf213b4e0bdd84445
zendo <linzway@qq.com>
2024-11-11 13:46:39 +0800

    treewide: format by vscode

--- dotfiles/bin/iommu_check.sh
+++ dotfiles/bin/iommu_check.sh
@@ -4,5 +4,5 @@ for g in $(find /sys/kernel/iommu_groups/* -maxdepth 0 -type d | sort -V); do
     echo "IOMMU Group ${g##*/}:"
     for d in $g/devices/*; do
         echo -e "\t$(lspci -nns ${d##*/})"
-    done;
-done;
+    done
+done
