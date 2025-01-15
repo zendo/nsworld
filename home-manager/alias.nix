f13c0ba493efdd17de2f2e1e884a0964db51b0cd
zendo <linzway@qq.com>
2025-01-15 08:50:36 +0800

    cleanup alias

--- home-manager/alias.nix
+++ home-manager/alias.nix
@@ -14,19 +14,17 @@
     rm-restore = "${pkgs.trash-cli}/bin/trash-restore";
     free = "free -h";
     bc = "${lib.getExe pkgs.libqalculate}";
+    inxi = "inxi -Fz";
+    ip = "ip --color=auto";
+    ip-info = "curl ip.im/info";
+    nload = "${lib.getExe pkgs.nload} devices wlp1s0";
     psp = "${lib.getExe pkgs.procs} --sortd UsageMem";
     ps-sort = ''ps -ewwo pid,%cpu,%mem,nice,pri,rtprio,args --sort=-pcpu,-pid | awk -v filter="$1" 'NR==1 || tolower($0) ~ tolower(filter)' | less -e --header=1'';
     dd-log = "sudo dd bs=8M oflag=sync,direct conv=fsync status=progress";
     mount-ls = "mount | column -t";
     beep = ''echo -en "\007"'';
+    journalctl-1h = ''journalctl -p err..alert --since "60 min ago"'';
 
-    inxi = "inxi -Fz";
-    ip = "ip --color=auto";
-    ip-info = "curl ip.im/info";
-    nload = "${lib.getExe pkgs.nload} devices wlp1s0";
-
-    where-am-i = "${pkgs.geoclue2}/libexec/geoclue-2.0/demos/where-am-i";
-    my-location = ''curl -s "https://location.services.mozilla.com/v1/geolocate?key=geoclue" | jq -r '"\(.location.lat):\(.location.lng)"' '';
     paste-termbin = "nc termbin.com 9999";
     paste-rs = "curl --data-binary @- https://paste.rs/";
 
@@ -35,7 +33,6 @@
     ee = "emacsclient --create-frame";
     ee-config = "emacs --init-directory ~/.config/emacs";
     ssr = "export {http,https,ftp}_proxy=socks5h://127.0.0.1:7897 ;export {HTTP,HTTPS,FTP}_PROXY=socks5h://127.0.0.1:7897";
-    journalctl-1h = ''journalctl -p err..alert --since "60 min ago"'';
     nix-build-package = ''nix build --impure --expr "(import <nixpkgs> {}).callPackage ./package.nix {}" -L'';
     nix-build-pr = ''f() { nix build github:NixOS/nixpkgs/pull/$1/merge#$2 }; f'';
   };
