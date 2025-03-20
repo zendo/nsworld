c9bc2f2bde4520938c3725a332731445ed5d8d0f
zendo <linzway@qq.com>
2025-03-20 17:04:26 +0800

    update

--- home-manager/alias.nix
+++ home-manager/alias.nix
@@ -27,6 +27,7 @@
 
     paste-termbin = "nc termbin.com 9999";
     paste-rs = "curl --data-binary @- https://paste.rs/";
+    download-sub-store = "wget https://github.com/sub-store-org/Sub-Store/releases/latest/download/sub-store.bundle.js";
     open-substore = "pushd ~/code/sub/sub-store > /dev/null && xdg-open http://127.0.0.1:3000 ; node sub-store.bundle.js & ; popd > /dev/null";
 
     nse = "nix search nixpkgs";
