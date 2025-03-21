fbaef282e7428e5b0242df362fb28d2fd1fc4474
zendo <linzway@qq.com>
2025-03-21 18:04:24 +0800

    emacs: shortcut rewrite

--- dotfiles/Windows/emacs-daemon.vbs
+++
@@ -1,2 +0,0 @@
-Set objShell = WScript.CreateObject("WScript.Shell")
-objShell.Run "cmd /c runemacs --daemon", 0, True 
--- dotfiles/Windows/emacsclient-shortcut.ps1
+++ dotfiles/Windows/emacsclient-shortcut.ps1
@@ -1,16 +1,31 @@
-param ( [string]$SourceExe, [string]$ArgumentsToSourceExe, [string]$DestinationPath )
+function Create-Shortcut {
+    param (
+        [string]$SourceExe,
+        [string]$ArgumentsToSourceExe,
+        [string]$DestinationPath
+    )
+    $WshShell = New-Object -COMObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut($DestinationPath)
+    $Shortcut.TargetPath = $SourceExe
+    $Shortcut.Arguments = $ArgumentsToSourceExe
+    $Shortcut.Save()
+}
 
-$DestinationPath = "$Home\Desktop\GNU Emacs.lnk"
-$SourceExe = "$Home\scoop\apps\emacs\current\bin\emacsclientw.exe"
-$ArgumentsToSourceExe = '-c -n -a ""'
+# 创建桌面快捷方式
+Create-Shortcut -SourceExe "$Home\scoop\apps\emacs\current\bin\emacsclientw.exe" `
+                -ArgumentsToSourceExe '-c -n -a ""' `
+                -DestinationPath "$Home\Desktop\GNU Emacs.lnk"
 
-$WshShell = New-Object -COMObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut($DestinationPath)
-$Shortcut.TargetPath = $SourceExe
-$Shortcut.Arguments = $ArgumentsToSourceExe
-$Shortcut.Save()
-
+# 创建启动项快捷方式
 # Win+r shell:startup
-Copy-Item ".\emacs-daemon.vbs" "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\"
+Create-Shortcut -SourceExe "$Home\scoop\apps\emacs\current\bin\runemacs.exe" `
+                -ArgumentsToSourceExe "--daemon" `
+                -DestinationPath "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\emacs-daemon.lnk"
 
-Write-Output "$DestinationPath is created successfully."
+# or VBScript
+# $Startup = 'Set objShell = WScript.CreateObject("WScript.Shell")
+# objShell.Run "cmd /c runemacs --daemon", 0, True
+# '
+# $Startup | Out-File "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\emacs-daemon.vbs"
+
+Write-Output "Shortcuts is created successfully."
