71d4b8ad9f3fa0253e00e3fe6130b7908a165c31
zendo <linzway@qq.com>
2025-03-26 22:27:15 +0800

    update

--- home-manager/alias.nix
+++ home-manager/alias.nix
@@ -6,6 +6,7 @@
 }:
 {
   home.shellAliases = {
+    # shell
     cat = "${lib.getExe pkgs.bat} -p";
     l = "${lib.getExe pkgs.eza} -l --icons";
     "la." = "ls -d .*";
@@ -13,31 +14,37 @@
     rm-empty = "${lib.getExe pkgs.gtrash} find --rm";
     rm-restore = "${lib.getExe pkgs.gtrash} restore";
     fcd = ''cd "$(find -type d | fzf --preview 'tree -C {} | head -200')"'';
-    free = "free -h";
     bc = "${lib.getExe pkgs.libqalculate}";
     inxi = "inxi -Fz";
-    ip = "ip --color=auto";
-    ip-info = "curl ip.im/info";
-    nload = "${lib.getExe pkgs.nload} devices wlp1s0";
+    free = "free -h";
+    mount-ls = "mount | column -t";
+    dd-log = "sudo dd bs=8M oflag=sync,direct conv=fsync status=progress";
     psp = "${lib.getExe pkgs.procs} --sortd UsageMem";
     ps-sort = ''ps -ewwo pid,%cpu,%mem,nice,pri,rtprio,args --sort=-pcpu,-pid | awk -v filter="$1" 'NR==1 || tolower($0) ~ tolower(filter)' | less -e --header=1'';
-    dd-log = "sudo dd bs=8M oflag=sync,direct conv=fsync status=progress";
-    mount-ls = "mount | column -t";
     beep = ''echo -en "\007"'';
     journalctl-1h = ''journalctl -p err..alert --since "60 min ago"'';
 
+    # network
+    ip = "ip --color=auto";
+    ip-info = "curl ip.im/info";
+    nload = "${lib.getExe pkgs.nload} devices wlp1s0";
+    ssr = "export {http,https,ftp}_proxy=socks5h://127.0.0.1:7897 ;export {HTTP,HTTPS,FTP}_PROXY=socks5h://127.0.0.1:7897";
+
+    # web
     paste-termbin = "nc termbin.com 9999";
     paste-rs = "curl --data-binary @- https://paste.rs/";
     download-sub-store = "wget https://github.com/sub-store-org/Sub-Store/releases/latest/download/sub-store.bundle.js";
     open-substore = "pushd ~/code/sub/sub-store > /dev/null && xdg-open http://127.0.0.1:3000 ; node sub-store.bundle.js & ; popd > /dev/null";
 
-    nse = "nix search nixpkgs";
-    j = "just --justfile=${config.home.homeDirectory}/nsworld/justfile";
+    # emacs
     e = "emacs -nw";
     ee = "emacsclient --create-frame";
     ee-config = "emacs --init-directory ~/.config/emacs";
-    magit = "emacsclient -n -c -e \(magit-status\)";
-    ssr = "export {http,https,ftp}_proxy=socks5h://127.0.0.1:7897 ;export {HTTP,HTTPS,FTP}_PROXY=socks5h://127.0.0.1:7897";
+    magit = ''emacsclient -n -c -e \(magit-status\)'';
+
+    # nix
+    nse = "nix search nixpkgs";
+    j = "just --justfile=${config.home.homeDirectory}/nsworld/justfile";
     nix-build-package = ''nix build --impure --expr "(import <nixpkgs> {}).callPackage ./package.nix {}" -L'';
     nix-build-pr = ''f() { nix build github:NixOS/nixpkgs/pull/$1/merge#$2 }; f'';
   };
