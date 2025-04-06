c3e5a4149443634936f09b5d04c701f7ce27582e
zendo <linzway@qq.com>
2025-04-06 21:06:59 +0800

    add anich

--- home-manager/gui.nix
+++ home-manager/gui.nix
@@ -7,6 +7,7 @@
 }:
 {
   home.packages = with pkgs; [
+    anich # 在线动漫弹幕
     cyc # 次元城动画
     # azcomicv
 
---
+++ pkgs/anich/package.nix
@@ -0,0 +1,63 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  dpkg,
+  wrapGAppsHook,
+  autoPatchelfHook,
+  udev,
+  libgbm,
+  cups,
+  nss,
+  nspr,
+  alsa-lib,
+  libdrm,
+  xorg,
+  mpv-unwrapped,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "anich";
+  version = "1.2.3";
+
+  src = fetchurl {
+    url = "https://github.com/Sle2p/AniCh/releases/download/${version}/anich-linux-${version}.deb";
+    hash = "sha256-R62kb26f5Y6itokwms6hl4htRPmhDpwHo7So15qe+ok=";
+  };
+
+  nativeBuildInputs = [
+    dpkg
+    wrapGAppsHook
+    autoPatchelfHook
+  ];
+
+  buildInputs = [
+    libgbm
+    cups
+    nss
+    nspr
+    alsa-lib
+    libdrm
+    xorg.libXv
+    mpv-unwrapped
+    (lib.getLib stdenv.cc.cc)
+  ];
+
+  runtimeDependencies = [ (lib.getLib udev) ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp -r usr/share $out
+    ln -s $out/share/ani/ani $out/bin
+  '';
+
+  meta = {
+    description = "(Anime Channel) 一个支持超分辨率的在线动漫弹幕APP";
+    homepage = "https://github.com/Sle2p/AniCh";
+    mainProgram = "ani";
+    platforms = [ "x86_64-linux" ];
+    license = lib.licenses.mit; # unknown
+    sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
+    maintainers = with lib.maintainers; [ zendo ];
+  };
+}
