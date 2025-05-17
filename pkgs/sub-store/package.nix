07638c16db3d38b169690e238c193d39627bd200
zendo <linzway@qq.com>
2025-05-17 23:22:40 +0800

    sub-store: update

--- pkgs/sub-store/package.nix
+++ pkgs/sub-store/package.nix
@@ -7,16 +7,16 @@
 
 stdenv.mkDerivation {
   pname = "sub-store";
-  version = "2.15.12";
+  version = "2.19.42";
 
   src = fetchurl {
     url = "https://github.com/sub-store-org/Sub-Store-Front-End/releases/latest/download/dist.zip";
-    sha256 = "sha256-o+L8W3tTJnyPghVgaMxMDcOTYwJYM3Zy0dsAbQiPOVM=";
+    sha256 = "sha256-0v0KmrQKhH6fiqO8h50PePRFKIiidkay+/SueYv235I=";
   };
 
   js = fetchurl {
     url = "https://github.com/sub-store-org/Sub-Store/releases/latest/download/sub-store.bundle.js";
-    sha256 = "sha256-khyRz5QC01lMsW0dnmY2OGCimJXRRzpyQgP+EMnGjHQ=";
+    sha256 = "sha256-HaCzX8PBFHwACqvTLwzabexcqIrK8S563m621qyjw1M=";
   };
 
   nativeBuildInputs = [ unzip ];
