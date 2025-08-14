46e27d6dfff9811464cddb55d3302f808aa19b45
zendo <linzway@qq.com>
2025-08-14 14:12:26 +0800

    ...

--- dotfiles/Windows/shortcut-emacsclient.ps1
+++ dotfiles/Windows/shortcut-emacsclient.ps1
@@ -11,17 +11,20 @@ function Create-Shortcut {
     $Shortcut.Save()
 }
 
+# C:\Program Files
+$programs = [Environment]::GetFolderPath("ProgramFiles")
+# Win+r shell:startup
+$startupFolder = [Environment]::GetFolderPath('Startup')
+
 # 创建桌面快捷方式
 # ((fullscreen . maximized))
 # ((top . 0)(left . -1)(height . 44)(width . 80))
-Create-Shortcut -SourceExe "$Home\scoop\apps\emacs\current\bin\emacsclientw.exe" `
+Create-Shortcut -SourceExe "emacsclientw.exe" `
                 -ArgumentsToSourceExe '-c -n -a "" -F "((top . 0)(left . -1)(height . 44)(width . 80))"' `
                 -DestinationPath "$Home\Desktop\GNU Emacs.lnk"
 
 # 创建启动项快捷方式
-# Win+r shell:startup
-$startupFolder = [Environment]::GetFolderPath('Startup')
-Create-Shortcut -SourceExe "$Home\scoop\apps\emacs\current\bin\runemacs.exe" `
+Create-Shortcut -SourceExe "runemacs.exe" `
                 -ArgumentsToSourceExe "--daemon" `
                 -DestinationPath "$startupFolder\emacs-daemon.lnk"
 
