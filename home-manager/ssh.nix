fbbf9c1dcd779216035c72747d518e0a22c4f397
zendo <linzway@qq.com>
2025-08-30 22:53:53 +0800

    ...
    ...
    
    ...

--- home-manager/ssh.nix
+++ home-manager/ssh.nix
@@ -2,6 +2,20 @@
 {
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
+    # default config
+    matchBlocks."*" = {
+      forwardAgent = false;
+      addKeysToAgent = "no";
+      compression = false;
+      serverAliveInterval = 0;
+      serverAliveCountMax = 3;
+      hashKnownHosts = false;
+      userKnownHostsFile = "~/.ssh/known_hosts";
+      controlMaster = "no";
+      controlPath = "~/.ssh/master-%r@%n:%p";
+      controlPersist = "no";
+    };
     extraConfig = ''
       Host github.com
         HostName ssh.github.com
