8966e15a6c99da85eeaebacaf578cada32be89ea
zendo <linzway@qq.com>
2025-10-18 04:01:42 +0800

    sing-box config refactor

--- overlays/flake-module.nix
+++ overlays/flake-module.nix
@@ -59,14 +59,14 @@
 
       # https://github.com/reF1nd/sing-box/tags
       # sing-box = prev.sing-box.overrideAttrs (oldAttrs: rec {
-      #   version = "1.12.9-reF1nd";
+      #   version = "1.12.10-reF1nd";
       #   src = prev.fetchFromGitHub {
       #     owner = "reF1nd";
       #     repo = "sing-box";
       #     tag = "v${version}";
-      #     hash = "sha256-GSUT9gA0X2FxFtTUCpKzDEZgNdshHkP8siWkx0mhoo8=";
+      #     hash = "sha256-s2TDZtVIgf/m3iJZHwJSNIsY98wHKua7Ld74szAn/pQ=";
       #   };
-      #   vendorHash = "sha256-BUFiYmxRlxzgxdl4bWxBsbvER/ilH4hW1k438A5y08E=";
+      #   vendorHash = "sha256-xsN+cdOFl7OS124xYj2EG1ZIz8MZV+lfbMPj1h+o55Q=";
       # });
 
       # xwayland env for inputMethod & native CSD
