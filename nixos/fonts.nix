9f6bde0e5842c7bbbc6cf5544944334ff83404e0
zendo <linzway@qq.com>
2025-10-21 21:37:46 +0800

    make `noto-fonts-color-emoji` installed surely

--- nixos/fonts.nix
+++ nixos/fonts.nix
@@ -11,6 +11,7 @@
         noto-fonts
         noto-fonts-cjk-sans
         noto-fonts-cjk-serif
+        noto-fonts-color-emoji
         adwaita-fonts
         # sarasa-gothic
         # lxgw-wenkai
