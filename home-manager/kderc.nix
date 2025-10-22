61b040c39e3749c5b2bf03dbb48305a2b6e829dd
zendo <linzway@qq.com>
2025-10-22 14:47:50 +0800

    rename

--- home-manager/kderc.nix
+++ home-manager/kderc.nix
@@ -1,6 +1,6 @@
 { inputs, ... }:
 {
-  imports = [ inputs.plasma-manager.homeManagerModules.plasma-manager ];
+  imports = [ inputs.plasma-manager.homeModules.plasma-manager ];
 
   programs.kate = {
     enable = true;
