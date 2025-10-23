f3bfe843fda83566df51fc1125e590b9267e37dc
zendo <linzway@qq.com>
2025-10-23 08:43:41 +0800

    remove patch

--- overlays/flake-module.nix
+++ overlays/flake-module.nix
@@ -101,23 +101,6 @@
         qt6final: qt6prev: {
           # `fcitx5-configtool` has a heavy dependency KDE Frameworks
           fcitx5-with-addons = qt6prev.fcitx5-with-addons.override { withConfigtool = false; };
-          # https://github.com/NixOS/nixpkgs/pull/454184
-          fcitx5-qt = qt6prev.fcitx5-qt.overrideAttrs (oldAttrs: {
-            patches = (oldAttrs.patches or [ ]) ++ [
-              (prev.fetchpatch2 {
-                url = "https://github.com/fcitx/fcitx5-qt/commit/46a07a85d191fd77a1efc39c8ed43d0cd87788d2.patch?full_index=1";
-                hash = "sha256-qv8Rj6YoFdMQLOB2R9LGgwCHKdhEji0Sg67W37jSIac=";
-              })
-              (prev.fetchpatch2 {
-                url = "https://github.com/fcitx/fcitx5-qt/commit/6ac4fdd8e90ff9c25a5219e15e83740fa38c9c71.patch?full_index=1";
-                hash = "sha256-x0OdlIVmwVuq2TfBlgmfwaQszXLxwRFVf+gEU224uVA=";
-              })
-              (prev.fetchpatch2 {
-                url = "https://github.com/fcitx/fcitx5-qt/commit/1d07f7e8d6a7ae8651eda658f87ab0c9df08bef4.patch?full_index=1";
-                hash = "sha256-22tKD7sbsTJcNqur9/Uf+XAvMvA7tzNQ9hUCMm+E+E0=";
-              })
-            ];
-          });
         }
       );
 
