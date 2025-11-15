e3faad70d7bd9fa933cba9da76052cc858429e9c
zendo <linzway@qq.com>
2025-11-15 09:32:39 +0800

    add cache

--- home-manager/hm-non-nixos.nix
+++ home-manager/hm-non-nixos.nix
@@ -52,6 +52,14 @@
         "flakes"
         "nix-command"
       ];
+      substituters = [
+        # "https://cache.nixos.org"
+        "https://cache.garnix.io"
+      ];
+      trusted-public-keys = [
+        # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      ];
     };
   };
 
