5ebdf812aa26fc10262b96241b6da878a6f9949f
zendo <linzway@qq.com>
2025-11-25 22:20:47 +0800

    add route rule

--- hosts/yoga/configuration.nix
+++ hosts/yoga/configuration.nix
@@ -60,10 +60,10 @@
   ];
 
   systemd.services.sub-store.enable = true;
-  # systemd.services.sing-box.enable = true;
+  systemd.services.sing-box.enable = true;
 
   services.mihomo = {
-    enable = true;
+    # enable = true;
     tunMode = true;
     webui = pkgs.zashboard;
     configFile = "/home/${username}/code/sub/config.yaml";
