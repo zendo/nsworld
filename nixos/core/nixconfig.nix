329d7e641637ced6b7f82652179e99fce66235d8
zendo <linzway@qq.com>
2025-11-28 22:44:15 +0800

    order

--- nixos/core/nixconfig.nix
+++ nixos/core/nixconfig.nix
@@ -37,13 +37,13 @@
         # "https://mirror.sjtu.edu.cn/nix-channels/store"
         # "https://mirrors.ustc.edu.cn/nix-channels/store"
         # "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-        # "https://nix-community.cachix.org"
         "https://cache.garnix.io"
+        # "https://nix-community.cachix.org"
       ];
 
       trusted-public-keys = [
-        # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+        # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
 
       trusted-users = [
