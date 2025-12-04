c00febec761305d80e39099541b0ddc1cb7d31ef
zendo <linzway@qq.com>
2025-12-04 22:23:38 +0800

    ghostty-cursor-shaders

--- overlays/default.nix
+++ overlays/default.nix
@@ -38,14 +38,6 @@
     modifications = final: prev: {
       # foo = prev.callPackage ./foo/package.nix { };
 
-      # nurl https://github.com/sahaj-b/ghostty-cursor-shaders
-      ghostty-cursor-shaders = prev.fetchFromGitHub {
-        owner = "sahaj-b";
-        repo = "ghostty-cursor-shaders";
-        rev = "14f7cd035f1b483dfe46ba11aef7377b6c4c687d";
-        hash = "sha256-ky343PVJklT4MqF5whULGwC5e5YfKF7PRGjB+CoBVUI=";
-      };
-
       # niri = prev.niri.overrideAttrs (
       #   finalAttrs: prevAttrs: {
       #     # version = "25.08-unstable-2025-11-24";
---
+++ pkgs/ghostty-cursor-shaders/package.nix
@@ -0,0 +1,9 @@
+{ fetchFromGitHub }:
+
+# nurl https://github.com/sahaj-b/ghostty-cursor-shaders
+fetchFromGitHub {
+  owner = "sahaj-b";
+  repo = "ghostty-cursor-shaders";
+  rev = "14f7cd035f1b483dfe46ba11aef7377b6c4c687d";
+  hash = "sha256-ky343PVJklT4MqF5whULGwC5e5YfKF7PRGjB+CoBVUI=";
+}
