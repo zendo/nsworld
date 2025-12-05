61c921db2687a853472c11a366a596a6221fc50c
zendo <linzway@qq.com>
2025-12-05 15:51:19 +0800

    clean

--- overlays/default.nix
+++ overlays/default.nix
@@ -109,16 +109,6 @@
       };
 
       # -----------------------------------------------------------------------
-      # qt6Packages overrideScope
-      # -----------------------------------------------------------------------
-      qt6Packages = prev.qt6Packages.overrideScope (
-        qt6final: qt6prev: {
-          # Disable `fcitx5-configtool` to avoid pulling in KDE frameworks dependencies
-          fcitx5-with-addons = qt6prev.fcitx5-with-addons.override { withConfigtool = false; };
-        }
-      );
-
-      # -----------------------------------------------------------------------
       # Python Module Overlays
       # -----------------------------------------------------------------------
       # nix build .#python3Packages.pyjokes
@@ -145,55 +135,67 @@
       #   night-theme-switcher = prev.callPackage ./night-theme-switcher { };
       # };
 
+      # -----------------------------------------------------------------------
+      # qt6Packages overrideScope
+      # -----------------------------------------------------------------------
+      qt6Packages = prev.qt6Packages.overrideScope (
+        qt6final: qt6prev: {
+          # Disable `fcitx5-configtool` to avoid pulling in KDE frameworks dependencies
+          fcitx5-with-addons = qt6prev.fcitx5-with-addons.override { withConfigtool = false; };
+        }
+      );
+
+      # -----------------------------------------------------------------------
+      # kdePackages overrideScope
+      # -----------------------------------------------------------------------
       # https://github.com/NixOS/nixpkgs/issues/126590#issuecomment-3194531220
-      kdePackages = prev.kdePackages // {
-        plasma-workspace =
-          let
-
-            # the package we want to override
-            basePkg = prev.kdePackages.plasma-workspace;
-
-            # a helper package that merges all the XDG_DATA_DIRS into a single directory
-            xdgdataPkg = prev.pkgs.stdenv.mkDerivation {
-              name = "${basePkg.name}-xdgdata";
-              buildInputs = [ basePkg ];
-              dontUnpack = true;
-              dontFixup = true;
-              dontWrapQtApps = true;
-              installPhase = ''
-                mkdir -p $out/share
-                ( IFS=:
-                  for DIR in $XDG_DATA_DIRS; do
-                    if [[ -d "$DIR" ]]; then
-                      cp -r $DIR/. $out/share/
-                      chmod -R u+w $out/share
+      kdePackages = prev.kdePackages.overrideScope (
+        kfinal: kprev: {
+          plasma-workspace =
+            let
+              # the package we want to override
+              basePkg = prev.kdePackages.plasma-workspace;
+              # a helper package that merges all the XDG_DATA_DIRS into a single directory
+              xdgdataPkg = prev.pkgs.stdenv.mkDerivation {
+                name = "${basePkg.name}-xdgdata";
+                buildInputs = [ basePkg ];
+                dontUnpack = true;
+                dontFixup = true;
+                dontWrapQtApps = true;
+                installPhase = ''
+                  mkdir -p $out/share
+                  ( IFS=:
+                    for DIR in $XDG_DATA_DIRS; do
+                      if [[ -d "$DIR" ]]; then
+                        cp -r $DIR/. $out/share/
+                        chmod -R u+w $out/share
+                      fi
+                    done
+                  )
+                '';
+              };
+              # undo the XDG_DATA_DIRS injection that is usually done in the qt wrapper
+              # script and instead inject the path of the above helper package
+              derivedPkg = basePkg.overrideAttrs {
+                preFixup = ''
+                  for index in "''${!qtWrapperArgs[@]}"; do
+                    if [[ ''${qtWrapperArgs[$((index+0))]} == "--prefix" ]] && [[ ''${qtWrapperArgs[$((index+1))]} == "XDG_DATA_DIRS" ]]; then
+                      unset -v "qtWrapperArgs[$((index+0))]"
+                      unset -v "qtWrapperArgs[$((index+1))]"
+                      unset -v "qtWrapperArgs[$((index+2))]"
+                      unset -v "qtWrapperArgs[$((index+3))]"
                     fi
                   done
-                )
-              '';
-            };
+                  qtWrapperArgs=("''${qtWrapperArgs[@]}")
+                  qtWrapperArgs+=(--prefix XDG_DATA_DIRS : "${xdgdataPkg}/share")
+                  qtWrapperArgs+=(--prefix XDG_DATA_DIRS : "$out/share")
+                '';
+              };
+            in
+            derivedPkg;
+        }
+      );
 
-            # undo the XDG_DATA_DIRS injection that is usually done in the qt wrapper
-            # script and instead inject the path of the above helper package
-            derivedPkg = basePkg.overrideAttrs {
-              preFixup = ''
-                for index in "''${!qtWrapperArgs[@]}"; do
-                  if [[ ''${qtWrapperArgs[$((index+0))]} == "--prefix" ]] && [[ ''${qtWrapperArgs[$((index+1))]} == "XDG_DATA_DIRS" ]]; then
-                    unset -v "qtWrapperArgs[$((index+0))]"
-                    unset -v "qtWrapperArgs[$((index+1))]"
-                    unset -v "qtWrapperArgs[$((index+2))]"
-                    unset -v "qtWrapperArgs[$((index+3))]"
-                  fi
-                done
-                qtWrapperArgs=("''${qtWrapperArgs[@]}")
-                qtWrapperArgs+=(--prefix XDG_DATA_DIRS : "${xdgdataPkg}/share")
-                qtWrapperArgs+=(--prefix XDG_DATA_DIRS : "$out/share")
-              '';
-            };
-
-          in
-          derivedPkg;
-      };
       # etc...
     }; # end of modifications
   }; # end of flake.overlays
