952159677774e7583246905890e88ce4fbc9ff6c
zendo <linzway@qq.com>
2025-12-06 10:59:47 +0800

    mkhost refactor

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,103 +1,106 @@
 { self, inputs, ... }:
-{
-  flake.nixosConfigurations =
-    let
-      mkHost =
-        {
-          myvars ? {
-            user = "";
-            host = "";
-          },
-          nixpkgs ? inputs.nixpkgs,
-          extraModules ? [ ],
-        }:
-        nixpkgs.lib.nixosSystem {
-          specialArgs = {
-            inherit inputs self myvars;
-          };
-          modules = [
-            (
-              { lib, myvars, ... }:
-              {
-                networking.hostName = myvars.host;
-                i18n.defaultLocale = lib.mkDefault "zh_CN.UTF-8";
-                time.timeZone = lib.mkDefault "Asia/Shanghai";
-                system.stateVersion = lib.mkDefault "26.05";
-              }
-            )
-          ]
-          # and more modules
-          ++ extraModules;
-        };
-    in
+let
+  mkHost =
     {
-      yoga = mkHost {
-        myvars = {
-          user = "iab";
-          host = "yoga";
-        };
-        extraModules = [
-          ./yoga/configuration.nix
-        ];
+      myvars ? { },
+      nixpkgs ? inputs.nixpkgs,
+      extraModules ? [ ],
+    }:
+    let
+      defaultVars = {
+        user = "";
+        host = "";
+        state = "26.05";
+        locale = "zh_CN.UTF-8";
+        timezone = "Asia/Shanghai";
       };
-
-      svp = mkHost {
-        myvars = {
-          user = "zendo";
-          host = "svp";
-        };
-        # nixpkgs = inputs.nixpkgs-stable;
-        extraModules = [
-          ./svp/configuration.nix
-        ];
+      vars = defaultVars // myvars;
+    in
+    nixpkgs.lib.nixosSystem {
+      specialArgs = {
+        inherit inputs self myvars;
       };
-
-      rmt = mkHost {
-        myvars = {
-          user = "aaa";
-          host = "rmt";
-        };
-        extraModules = [
-          ./rmt/configuration.nix
-        ];
-      };
-
-      # nix build .#nixosConfigurations.vmtest.config.system.build.vm
-      vmtest = mkHost {
-        myvars = {
-          user = "test";
-          host = "vmtest";
-        };
-        extraModules = [
-          ./vmtest/configuration.nix
-        ];
-      };
-
-      # sudo nix run .#nixosConfigurations.wsl.config.system.build.tarballBuilder
-      wsl = mkHost {
-        myvars = {
-          user = "iab";
-          host = "wsl";
-        };
-        extraModules = [
-          ./wsl/configuration.nix
-        ];
-      };
-
-      # nix build .#nixosConfigurations.livecd-standard.config.system.build.isoImage
-      livecd-standard = mkHost {
-        myvars = {
-          user = "live";
-          host = "livecd";
-        };
-        extraModules = [
-          ./livecd/standard.nix
-        ];
-      };
-
-      # nix build .#nixosConfigurations.livecd-minimal.config.system.build.isoImage
-      livecd-minimal = inputs.nixpkgs.lib.nixosSystem {
-        modules = [ ./livecd/minimal.nix ];
-      };
+      modules = [
+        {
+          networking.hostName = vars.host;
+          i18n.defaultLocale = vars.locale;
+          time.timeZone = vars.timezone;
+          system.stateVersion = vars.state;
+        }
+      ]
+      # and more modules
+      ++ extraModules;
     };
+in
+{
+  flake.nixosConfigurations = {
+    yoga = mkHost {
+      myvars = {
+        user = "iab";
+        host = "yoga";
+      };
+      extraModules = [
+        ./yoga/configuration.nix
+      ];
+    };
+
+    svp = mkHost {
+      myvars = {
+        user = "zendo";
+        host = "svp";
+      };
+      # nixpkgs = inputs.nixpkgs-stable;
+      extraModules = [
+        ./svp/configuration.nix
+      ];
+    };
+
+    rmt = mkHost {
+      myvars = {
+        user = "aaa";
+        host = "rmt";
+      };
+      extraModules = [
+        ./rmt/configuration.nix
+      ];
+    };
+
+    # nix build .#nixosConfigurations.vmtest.config.system.build.vm
+    vmtest = mkHost {
+      myvars = {
+        user = "test";
+        host = "vmtest";
+      };
+      extraModules = [
+        ./vmtest/configuration.nix
+      ];
+    };
+
+    # sudo nix run .#nixosConfigurations.wsl.config.system.build.tarballBuilder
+    wsl = mkHost {
+      myvars = {
+        user = "iab";
+        host = "wsl";
+      };
+      extraModules = [
+        ./wsl/configuration.nix
+      ];
+    };
+
+    # nix build .#nixosConfigurations.livecd-standard.config.system.build.isoImage
+    livecd-standard = mkHost {
+      myvars = {
+        user = "live";
+        host = "livecd";
+      };
+      extraModules = [
+        ./livecd/standard.nix
+      ];
+    };
+
+    # nix build .#nixosConfigurations.livecd-minimal.config.system.build.isoImage
+    livecd-minimal = inputs.nixpkgs.lib.nixosSystem {
+      modules = [ ./livecd/minimal.nix ];
+    };
+  };
 }
