3f0a6423de0aceaff8ad665a3fc8e3e74db71d27
zendo <linzway@qq.com>
2025-12-16 07:52:27 +0800

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'disko':
        'github:nix-community/disko/d64e5cdca35b5fad7c504f615357a7afe6d9c49e?narHash=sha256-fTLX9kDwLr9Y0rH/nG%2Bh1XG5UU%2BjBcy0PFYn5eneRX8%3D' (2025-12-10)
      → 'github:nix-community/disko/7194cfe5b7a3660726b0fe7296070eaef601cae9?narHash=sha256-YD5QWlGnusNbZCqR3pxG8tRxx9yUXayLZfAJRWspq2s%3D' (2025-12-15)
    • Updated input 'flake-parts':
        'github:hercules-ci/flake-parts/5635c32d666a59ec9a55cab87e898889869f7b71?narHash=sha256-MhA7wmo/7uogLxiewwRRmIax70g6q1U/YemqTGoFHlM%3D' (2025-12-11)
      → 'github:hercules-ci/flake-parts/a34fae9c08a15ad73f295041fec82323541400a9?narHash=sha256-XswHlK/Qtjasvhd1nOa1e8MgZ8GS//jBoTqWtrS1Giw%3D' (2025-12-15)
    • Updated input 'flake-parts/nixpkgs-lib':
        'github:nix-community/nixpkgs.lib/719359f4562934ae99f5443f20aa06c2ffff91fc?narHash=sha256-b0yj6kfvO8ApcSE%2BQmA6mUfu8IYG6/uU28OFn4PaC8M%3D' (2025-10-29)
      → 'github:nix-community/nixpkgs.lib/2075416fcb47225d9b68ac469a5c4801a9c4dd85?narHash=sha256-k00uTP4JNfmejrCLJOwdObYC9jHRrr/5M/a/8L2EIdo%3D' (2025-12-14)
    • Updated input 'home-manager':
        'github:nix-community/home-manager/39cb677ed9e908e90478aa9fe5f3383dfc1a63f3?narHash=sha256-HlbvQAqLx7WqZFFQZ8nu5UUJAVlXiV/kqKbyueA8srw%3D' (2025-12-11)
      → 'github:nix-community/home-manager/8315c1544f383b791a3115c9959d1f27920e8320?narHash=sha256-tyNJjd48hfgsyEfsq1Ueufg4oJv6b8xBA6NYRJrLPyg%3D' (2025-12-15)
    • Updated input 'nixos-wsl':
        'github:nix-community/NixOS-WSL/0c040f28b44b18e0d4240e027096078e34dbb029?narHash=sha256-w6wznH1lBzlSH3%2BpWDkE%2BL6xA0F02drFAzu2E7PD/Jo%3D' (2025-12-11)
      → 'github:nix-community/NixOS-WSL/be4af8042e7a61fa12fda58fe9a3b3babdefe17b?narHash=sha256-55V0AJ36V5Egh4kMhWtDh117eE3GOjwq5LhwxDn9eHg%3D' (2025-12-15)
    • Updated input 'nixpkgs':
        'https://releases.nixos.org/nixos/unstable/nixos-26.05pre910362.2fbfb1d73d23/nixexprs.tar.xz?narHash=sha256-yMgWBfeR/K9Tdk7wSxLZHJ%2BUUCuPVzu/rk4Ujtp0kDA%3D' (2025-12-11)
      → 'https://releases.nixos.org/nixos/unstable/nixos-26.05pre912002.1306659b587d/nixexprs.tar.xz?narHash=sha256-2BzIb1uB1hPl5fKGsMc%2Bxj2DrSYmpRLcKdCq0UHdLwI%3D' (2025-12-15)
    • Updated input 'sops-nix':
        'github:Mic92/sops-nix/496a6f625f63b780ce849891868f2fad22fd49c6?narHash=sha256-Ii16Nq5jL2wURXpV3D3tOM3vPpbKh18roHLkyZCHK4Q%3D' (2025-12-12)
      → 'github:Mic92/sops-nix/443a7f2e7e118c4fc63b7fae05ab3080dd0e5c63?narHash=sha256-hWRYfdH2ONI7HXbqZqW8Q1y9IRbnXWvtvt/ONZovSNY%3D' (2025-12-15)
    
    Nixpkgs Comparing changes: https://github.com/NixOS/nixpkgs/compare/2fbfb1d73d239d2402a8fe03963e37aab15abe8b...1306659b587dc277866c7b69eb97e5f07864d8c4

--- modules/home/wm/shikane.nix
+++ modules/home/wm/shikane.nix
@@ -5,13 +5,52 @@
     shikane
   ];
 
-  # wdisplays set the right outputs
+  # wdisplays // set the right outputs
   # install -Dm644 /dev/null ~/.config/shikane/config.toml
-  # shikane
-  # shikanectl export "host"
+  # shikane &
+  # shikanectl export $(uname -n)
+  # fg shikane
   services.shikane = {
     enable = true;
+    settings = {
+      profile = [
+        {
+          name = "yoga";
+          output = [
+            {
+              enable = true;
+              search = [
+                "m=0x8A90"
+                "s="
+                "v=Lenovo Group Limited"
+              ];
+              mode = "2880x1800@90Hz";
+              position = "0,0";
+              scale = 2;
+              transform = "normal";
+              adaptive_sync = false;
+            }
+          ];
+        }
+        {
+          name = "svp";
+          output = [
+            {
+              enable = true;
+              search = [
+                "m=VVX13F009G00"
+                "s="
+                "v=Panasonic Industry Company"
+              ];
+              mode = "1920x1080@60Hz";
+              position = "0,0";
+              scale = 1.5;
+              transform = "normal";
+              adaptive_sync = false;
+            }
+          ];
+        }
+      ];
+    };
   };
-
-  xdg.configFile."shikane".source = ../../../dotfiles/shikane;
 }
