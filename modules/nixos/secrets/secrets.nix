6891056a749c31feccef8c568ef2f9dbd419eaec
zendo <linzway@qq.com>
2025-12-21 15:28:26 +0800

    ...

--- modules/nixos/secrets/secrets.nix
+++ modules/nixos/secrets/secrets.nix
@@ -1,15 +1,14 @@
 /*
   mkdir -p ~/.config/sops/age
-  # Using own private ssh key
-  ssh-to-age -private-key -i ~/.ssh/id_ed25519 > ~/.config/sops/age/keys.txt
-  # or Remote host [sshKeyPaths]
+
+  # Using own private ssh key [keyFile]
+  ssh-to-age -private-key -i ~/.ssh/id_ed25519 > ~/.config/sops/age/keys.txt | age-keygen -y
+  # and host ssh key [sshKeyPaths]
   cat /etc/ssh/ssh_host_ed25519_key.pub | ssh-to-age
-  sudo ssh-to-age -private-key -i /etc/ssh/ssh_host_ed25519_key | age-keygen -y
 
-  # Print out public key and add in .sops.yaml
-  age-keygen -y ~/.config/sops/age/keys.txt
+  # Paste into .sops.yaml
 
-  # Update all secrets when add new key
+  # Update all secrets when key changed
   sops updatekeys modules/nixos/secrets/secrets.yaml
 
   # Decrypt and show the real value
@@ -23,7 +22,6 @@
     defaultSopsFile = ./secrets.yaml;
     age = {
       sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-      # keyFile = "/home/username/.config/sops/age/keys.txt";
     };
   };
 }
