917d78a9d0b3da47da7ea770f0998a1cf30f1343
zendo <linzway@qq.com>
2025-12-31 03:04:24 +0800

    mime

--- modules/home/xdg/mime.nix
+++ modules/home/xdg/mime.nix
@@ -3,30 +3,40 @@
   nixosConfig,
   ...
 }:
+let
+  music = "audacious.desktop";
+  browser = "firefox.desktop";
+  gtk-image = "org.gnome.Loupe.desktop";
+  qt-image = "org.kde.gwenview.desktop";
+in
 {
   # https://www.iana.org/assignments/media-types/media-types.xhtml
   xdg.mimeApps = {
     enable = true;
     defaultApplications = lib.mkMerge [
       {
-        "audio/mpeg" = "audacious.desktop";
-        "audio/flac" = "audacious.desktop";
-        "audio/x-vorbis+ogg" = "audacious.desktop";
-        "text/html" = "firefox.desktop";
-        "x-scheme-handler/http" = "firefox.desktop";
-        "x-scheme-handler/https" = "firefox.desktop";
-        "image/jpeg" = "org.gnome.Loupe.desktop";
-        "image/png" = "org.gnome.Loupe.desktop";
-        "image/webp" = "org.gnome.Loupe.desktop";
+        "audio/mpeg" = music;
+        "audio/flac" = music;
+        "audio/x-vorbis+ogg" = music;
+
+        "text/html" = browser;
+        "x-scheme-handler/http" = browser;
+        "x-scheme-handler/https" = browser;
+
+        "image/jpeg" = gtk-image;
+        "image/png" = gtk-image;
+        "image/webp" = gtk-image;
+
         "application/pdf" = "org.gnome.Papers.desktop";
       }
       (lib.mkIf nixosConfig.services.desktopManager.gnome.enable {
         "text/plain" = "org.gnome.TextEditor.desktop";
       })
       (lib.mkIf nixosConfig.services.desktopManager.plasma6.enable {
-        "image/jpeg" = "org.kde.gwenview.desktop";
-        "image/png" = "org.kde.gwenview.desktop";
-        "image/webp" = "org.kde.gwenview.desktop";
+        "image/jpeg" = qt-image;
+        "image/png" = qt-image;
+        "image/webp" = qt-image;
+
         "text/plain" = "org.kde.kwrite.desktop";
         "application/pdf" = "org.kde.okular.desktop";
       })
