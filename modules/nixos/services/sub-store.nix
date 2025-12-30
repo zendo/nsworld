08e7d75b79cb25588f4a0b4e598c8c048345362e
zendo <linzway@qq.com>
2025-12-31 03:31:55 +0800

    fix

--- modules/nixos/services/sub-store.nix
+++ modules/nixos/services/sub-store.nix
@@ -4,23 +4,27 @@
   ...
 }:
 {
+  # https://surge.tel/08/2930
+  # https://hub.docker.com/r/xream/sub-store
   # FRONTEND: http://127.0.0.1:3001/subs
   # BACKEND: curl -O "http://127.0.0.1:3000/download/collection/all?target=sing-box"
   systemd.services.sub-store = {
     enable = lib.mkDefault false;
-    after = [ "network.target" ];
-    wants = [ "network.target" ];
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
+    environment = {
+      # SUB_STORE_BACKEND_SYNC_CRON = "55 23 * * *"; # 23:55
+      # SUB_STORE_FRONTEND_BACKEND_PATH = "/2cXaAxRGfddmGz2yx1wA";
+      SUB_STORE_DATA_BASE_PATH = "%S/sub-store";
+      SUB_STORE_FRONTEND_PATH = "${pkgs.sub-store}/frontend";
+      SUB_STORE_BACKEND_API_HOST = "127.0.0.1";
+      SUB_STORE_BACKEND_API_PORT = "3000";
+      SUB_STORE_FRONTEND_HOST = "127.0.0.1";
+      SUB_STORE_FRONTEND_PORT = "3001";
+    };
     serviceConfig = {
       StateDirectory = "sub-store";
       StateDirectoryMode = "0700";
-      Environment = [
-        "SUB_STORE_DATA_BASE_PATH=/var/lib/sub-store"
-        "SUB_STORE_FRONTEND_PATH=${pkgs.sub-store}/frontend"
-        "SUB_STORE_BACKEND_API_HOST=127.0.0.1"
-        "SUB_STORE_BACKEND_API_PORT=3000"
-        "SUB_STORE_FRONTEND_HOST=127.0.0.1"
-        "SUB_STORE_FRONTEND_PORT=3001"
-      ];
       ExecStart = "${lib.getExe pkgs.nodejs} ${pkgs.sub-store}/sub-store.bundle.js";
       Restart = "on-failure";
       DynamicUser = true;
