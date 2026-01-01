d1f0f3fb820a38539bc59f52ed570ce4b2e431d5
zendo <linzway@qq.com>
2026-01-01 09:08:32 +0800

    del ghostty-cursor-shaders

--- packages/ghostty-cursor-shaders/package.nix
+++
@@ -1,9 +0,0 @@
-{ fetchFromGitHub }:
-
-# nurl https://github.com/sahaj-b/ghostty-cursor-shaders
-fetchFromGitHub {
-  owner = "sahaj-b";
-  repo = "ghostty-cursor-shaders";
-  rev = "14f7cd035f1b483dfe46ba11aef7377b6c4c687d";
-  hash = "sha256-ky343PVJklT4MqF5whULGwC5e5YfKF7PRGjB+CoBVUI=";
-}
