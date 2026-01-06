ec829d2b2c8179ccf8d6e1d759ac90c89bf4486d
zendo <linzway@qq.com>
2026-01-06 23:33:33 +0800

    del umport

--- modules/lib/default.nix
+++ modules/lib/default.nix
@@ -4,11 +4,10 @@
     final: prev:
     let
       attrsets = import ./attrsets.nix { lib = final; };
-      umport = import ./umport.nix { lib = final; };
       paths = import ./paths.nix { lib = final; };
     in
     {
-      custom = attrsets // umport // paths;
+      custom = attrsets // paths;
     }
   );
 }
