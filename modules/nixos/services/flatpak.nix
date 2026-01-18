b31b55b0dd081ff7052fbb9c7a61d7e41e65d92c
zendo <linzway@qq.com>
2026-01-18 22:16:38 +0800

    del flatpak-repo service

--- modules/nixos/services/flatpak.nix
+++ modules/nixos/services/flatpak.nix
@@ -1,17 +1,6 @@
-{ config, pkgs, ... }:
+{ config, ... }:
 {
+  # flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
   # flatpak install flathub io.github.kolunmi.Bazaar
   services.flatpak.enable = config.services.graphical-desktop.enable;
-
-  systemd.services.flatpak-repo = {
-    enable = config.services.flatpak.enable;
-    wantedBy = [ "multi-user.target" ];
-    path = [ pkgs.flatpak ];
-    serviceConfig = {
-      Type = "oneshot";
-    };
-    script = ''
-      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    '';
-  };
 }
