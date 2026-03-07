d719f0527e979dff032b5580f31000da68598b3a
zendo <linzway@qq.com>
2026-03-08 02:30:56 +0800

    agenix more

--- modules/nixos/secrets/agenix.nix
+++ modules/nixos/secrets/agenix.nix
@@ -2,18 +2,22 @@
   https://github.com/ryantm/agenix
   https://fzakaria.com/2024/07/12/nix-secrets-for-dummies
   https://github.com/yaxitech/ragenix
+  https://github.com/oddlama/agenix-rekey
   https://github.com/GaetanLepage/nix-config/blob/master/modules/nixos/core/agenix.nix
 */
-{ inputs, self, ... }:
 {
-  # module
+  inputs,
+  self,
+  pkgs,
+  ...
+}:
+{
   imports = [
     inputs.agenix.nixosModules.default
   ];
 
-  # app
   environment.systemPackages = [
-    inputs.agenix.packages.x86_64-linux.default
+    pkgs.ragenix
   ];
 
   # sudo ls /run/agenix/
