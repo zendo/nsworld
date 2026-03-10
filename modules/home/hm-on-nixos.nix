d3eb3443c55a38b9d57e6642b2b7aa9e275b4c6a
zendo <linzway@qq.com>
2026-03-10 23:33:45 +0800

    ...

--- modules/home/hm-on-nixos.nix
+++ modules/home/hm-on-nixos.nix
@@ -7,9 +7,15 @@
 }:
 {
   # agenix
+  # sudo cp /etc/ssh/ssh_host_ed25519_key  ~/.ssh/id_ed25519
+  # sudo cp /etc/ssh/ssh_host_ed25519_key.pub ~/.ssh/id_ed25519.pub
+  # sudo chown -R $USER  ~/.ssh
   age.identityPaths = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
 
   # sops-nix
+  # mkdir -p ~/.config/sops/age
+  # sudo ssh-to-age -private-key -i /etc/ssh/ssh_host_ed25519_key > ~/.config/sops/age/keys.txt
+  # chmod 600 ~/.config/sops/age/keys.txt
   sops.age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
 
   imports = [
