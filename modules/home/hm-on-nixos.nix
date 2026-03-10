d57b1daeba1d7261db0fde90e73e10286e8194a5
zendo <linzway@qq.com>
2026-03-10 23:37:40 +0800

    ...

--- modules/home/hm-on-nixos.nix
+++ modules/home/hm-on-nixos.nix
@@ -6,24 +6,7 @@
   ...
 }:
 {
-  # agenix
-  # sudo cp /etc/ssh/ssh_host_ed25519_key  ~/.ssh/id_ed25519
-  # sudo cp /etc/ssh/ssh_host_ed25519_key.pub ~/.ssh/id_ed25519.pub
-  # sudo chown -R $USER  ~/.ssh
-  age.identityPaths = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
-
-  # sops-nix
-  # mkdir -p ~/.config/sops/age
-  # sudo ssh-to-age -private-key -i /etc/ssh/ssh_host_ed25519_key > ~/.config/sops/age/keys.txt
-  # chmod 600 ~/.config/sops/age/keys.txt
-  sops.age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
-
   imports = [
-    inputs.sops-nix.homeManagerModules.sops
-    ../secrets/sopsnix.nix
-    inputs.agenix.homeManagerModules.default
-    ../secrets/agenix.nix
-    # -------------------------------------
     ./editor/helix.nix
     ./editor/micro.nix
     # -------------------------------------
@@ -75,7 +58,28 @@
   # ===========================================================
   # GNOME specific configurations
   # ===========================================================
-  ++ lib.optionals nixosConfig.services.desktopManager.gnome.enable [ ./wm/dconf.nix ];
+  ++ lib.optionals nixosConfig.services.desktopManager.gnome.enable [ ./wm/dconf.nix ]
+  # ===========================================================
+  # Secrets Management
+  # ===========================================================
+  ++ [
+    inputs.sops-nix.homeManagerModules.sops
+    ../secrets/sopsnix.nix
+    inputs.agenix.homeManagerModules.default
+    ../secrets/agenix.nix
+  ];
+
+  # agenix
+  # sudo cp /etc/ssh/ssh_host_ed25519_key  ~/.ssh/id_ed25519
+  # sudo cp /etc/ssh/ssh_host_ed25519_key.pub ~/.ssh/id_ed25519.pub
+  # sudo chown -R $USER  ~/.ssh
+  age.identityPaths = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
+
+  # sops-nix
+  # mkdir -p ~/.config/sops/age
+  # sudo ssh-to-age -private-key -i /etc/ssh/ssh_host_ed25519_key > ~/.config/sops/age/keys.txt
+  # chmod 600 ~/.config/sops/age/keys.txt
+  sops.age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
 
   # home.enableNixpkgsReleaseCheck = false;
   home.stateVersion = nixosConfig.system.stateVersion;
