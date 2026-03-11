bb7dd3426f7914b8b43406757accb6822af31008
zendo <linzway@qq.com>
2026-03-11 09:49:38 +0800

    ...

--- modules/home/hm-on-nixos.nix
+++ modules/home/hm-on-nixos.nix
@@ -77,7 +77,7 @@
 
   # sops-nix
   # mkdir -p ~/.config/sops/age
-  # sudo ssh-to-age -private-key -i /etc/ssh/ssh_host_ed25519_key > ~/.config/sops/age/keys.txt
+  # sudo ssh-to-age -private-key -i /etc/ssh/ssh_host_ed25519_key -o ~/.config/sops/age/keys.txt
   # chmod 600 ~/.config/sops/age/keys.txt
   sops.age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
 
