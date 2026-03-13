13ff392c578b54f991066634b9ff146715ebaecb
zendo <linzway@qq.com>
2026-03-14 07:28:21 +0800

    ssh config

--- modules/home/programs/ssh.nix
+++ modules/home/programs/ssh.nix
@@ -4,20 +4,27 @@
     enable = true;
     enableDefaultConfig = false;
     matchBlocks = {
-      # default config from:
-      # https://github.com/nix-community/home-manager/blob/master/modules/programs/ssh.nix#L648
       "*" = {
-        # setEnv.TERM = "xterm-256color";
-        forwardAgent = false;
-        addKeysToAgent = "no";
-        compression = false;
-        serverAliveInterval = 0;
-        serverAliveCountMax = 3;
-        hashKnownHosts = false;
-        userKnownHostsFile = "~/.ssh/known_hosts";
-        controlMaster = "no";
+        # --- 1. 保持连接（防掉线） ---
+        serverAliveInterval = 60; # 每 60 秒发一次心跳包
+        serverAliveCountMax = 5; # 连续 5 次没响应才断开
+        # --- 2. 自动化密钥管理 ---
+        addKeysToAgent = "yes"; # 第一次解密私钥后，自动存入 ssh-agent
+        forwardAgent = false; # 默认关闭转发，仅在特定 host 下开启
+        # --- 3. 连接多路复用（秒连神器） ---
+        # 第一次连接会建立 Master，后续连接同一台机器将直接复用，无需握手
+        controlMaster = "auto";
+        controlPersist = "30m"; # 主连接关闭后，后台保持 30 分钟
         controlPath = "~/.ssh/master-%r@%n:%p";
-        controlPersist = "no";
+        # --- 4. 性能与安全性平衡 ---
+        # hashKnownHosts = true; # 开启哈希，保护你的 known_hosts 不被扫描
+        # --- 5. 现代加密偏好 ---
+        extraOptions = {
+          # 优先使用更安全的算法
+          "HostKeyAlgorithms" = "ssh-ed25519,ssh-ed25519-cert-v01@openssh.com,rsa-sha2-512,rsa-sha2-256";
+          # 避免连接时弹出一堆无用的私钥尝试
+          "IdentitiesOnly" = "yes";
+        };
       };
       "github.com" = {
         hostname = "ssh.github.com";
@@ -36,6 +43,7 @@
       rmt = {
         hostname = "192.168.122.86";
         port = 22;
+        forwardAgent = true; # TODO
       };
     };
     extraConfig = ''
