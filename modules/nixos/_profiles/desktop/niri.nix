746b713ef708ef6fe60d45c9026ad38880e8b08d
zendo <linzway@qq.com>
2026-03-21 21:27:27 +0800

    all in wm.nix

--- modules/nixos/_profiles/desktop/niri.nix
+++
@@ -1,9 +0,0 @@
-{
-  flake.modules.nixos.niri =
-    { self, ... }:
-    {
-      imports = [ self.modules.nixos.wm ];
-      programs.niri.enable = true;
-      services.displayManager.dms-greeter.compositor.name = "niri";
-    };
-}
--- modules/nixos/_profiles/desktop/wm.nix
+++ modules/nixos/_profiles/desktop/wm.nix
@@ -8,6 +8,8 @@
         # self.modules.nixos.ly
       ];
 
+      home-manager.sharedModules = [ self.modules.homeManager.wm ];
+
       security = {
         soteria.enable = true; # Polkit agent
         pam.services.hyprlock = { };
@@ -20,16 +22,12 @@
         tumbler.enable = true; # thumbnailer service
       };
 
-      programs = {
-        light.enable = true;
-
-        thunar = {
-          enable = true;
-          plugins = with pkgs.xfce; [
-            thunar-archive-plugin
-            thunar-volman
-          ];
-        };
+      programs.thunar = {
+        enable = true;
+        plugins = with pkgs; [
+          thunar-archive-plugin
+          thunar-volman
+        ];
       };
 
       environment.pathsToLink = [
@@ -42,93 +40,197 @@
         # WLR_DRM_NO_ATOMIC = "1";
         WLR_NO_HARDWARE_CURSORS = "1";
       };
+    };
 
-      # =========================================================================
-      #  Home-manager
-      # =========================================================================
-      home-manager.sharedModules = [
-        (
-          { pkgs, ... }:
-          {
-            imports = [
-              ../../home/wm/hypridle.nix
-              ../../home/wm/shikane.nix
-            ];
-
-            home.packages = with pkgs; [
-              libnotify # notify-send
-              wlogout
-              swayidle
-              hypridle
-              hyprlock
-              stasis # idle manager
-              wf-recorder
-              blueberry
-              uwsm
-              xwayland-satellite
-              wev # wayland event view
-              # waypipe # proxy ?
-              # wtype # xdotool
-
-              # Display
-              avizo # lightctl / volumectl
-              wlsunset
-              brightnessctl # same like light
-              wl-gammactl
-              wlr-randr
-
-              # Media
-              # grim # grab image
-              # slurp # select region
-              nomacs
-              # pavucontrol
-              pwvucontrol # Pipewire Volume Control
-              playerctl # media player control
-              pamixer # volume control
-
-              # Needs when use other DM
-              adwaita-icon-theme
-              gnome-themes-extra
-              glib.dev # gsettings
-              dconf-editor
-
-              loupe
-              papers
-              # file-roller
-              xarchiver
-              # gnome.gnome-power-manager
-              # gnome.gnome-characters
-              # gthumb
-              # kdePackages.gwenview
-              gparted
-            ];
-
-            services = {
-              # network-manager-applet.enable = true;
-              # playerctld.enable = true;
-
-              cliphist = {
-                enable = true;
-                allowImages = true;
-              };
-            };
-
-            # qt = {
-            #   enable = true;
-            #   platformTheme.name = "adwaita";
-            #   style.name = "adwaita";
-            #   style.package = pkgs.adwaita-qt;
-            # };
-
-            xdg.dataFile = {
-              # Fix tiny cursor on qt apps
-              "icons/default/index.theme".text = ''
-                [icon theme]
-                Inherits=Adwaita
-              '';
-            };
-          }
-        )
+  # ╭─────────────────────────────────────────────────────╮
+  # │  Home-manager                                       │
+  # ╰─────────────────────────────────────────────────────╯
+  flake.modules.homeManager.wm =
+    { pkgs, ... }:
+    {
+      imports = [
+        ../../../home/wm/hypridle.nix
+        ../../../home/wm/shikane.nix
       ];
+
+      home.packages = with pkgs; [
+        libnotify # notify-send
+        wlogout
+        swayidle
+        hypridle
+        hyprlock
+        stasis # idle manager
+        wf-recorder
+        uwsm
+        xwayland-satellite
+        wev # wayland event view
+        # waypipe # proxy ?
+        # wtype # xdotool
+
+        # Display
+        avizo # lightctl / volumectl
+        brightnessctl
+        wlsunset
+        wl-gammactl
+        wlr-randr
+
+        # Media
+        # grim # grab image
+        # slurp # select region
+        nomacs
+        # pavucontrol
+        pwvucontrol # Pipewire Volume Control
+        playerctl # media player control
+        pamixer # volume control
+
+        # Needs when use other DM
+        adwaita-icon-theme
+        gnome-themes-extra
+        glib.dev # gsettings
+        dconf-editor
+
+        loupe
+        papers
+        # file-roller
+        xarchiver
+        # gnome.gnome-power-manager
+        # gnome.gnome-characters
+        # gthumb
+        # kdePackages.gwenview
+        gparted
+      ];
+
+      services = {
+        # network-manager-applet.enable = true;
+        # playerctld.enable = true;
+
+        cliphist = {
+          enable = true;
+          allowImages = true;
+        };
+      };
+
+      # qt = {
+      #   enable = true;
+      #   platformTheme.name = "adwaita";
+      #   style.name = "adwaita";
+      #   style.package = pkgs.adwaita-qt;
+      # };
+
+      xdg.dataFile = {
+        # Fix tiny cursor on qt apps
+        "icons/default/index.theme".text = ''
+          [icon theme]
+          Inherits=Adwaita
+        '';
+      };
+    };
+
+  # ╭──────────────────────────────────────────╮
+  # │ SWAY                                     │
+  # ╰──────────────────────────────────────────╯
+  flake.modules.nixos.sway =
+    { pkgs, self, ... }:
+    {
+      imports = [ self.modules.nixos.wm ];
+      programs.sway = {
+        enable = true;
+        wrapperFeatures.gtk = true;
+        extraPackages = [
+          pkgs.autotiling-rs
+        ];
+      };
+    };
+
+  # ╭──────────────────────────────────────────╮
+  # │ HYPRLAND                                 │
+  # ╰──────────────────────────────────────────╯
+  flake.modules.nixos.hyprland =
+    { self, ... }:
+    {
+      imports = [ self.modules.nixos.wm ];
+      programs.hyprland = {
+        enable = true;
+        withUWSM = true;
+      };
+    };
+
+  # ╭──────────────────────────────────────────╮
+  # │ NIRI                                     │
+  # ╰──────────────────────────────────────────╯
+  flake.modules.nixos.niri =
+    { self, ... }:
+    {
+      imports = [ self.modules.nixos.wm ];
+      programs.niri.enable = true;
+      services.displayManager.dms-greeter.compositor.name = "niri";
+    };
+
+  # ╭──────────────────────────────────────────╮
+  # │ GREETD                                   │
+  # ╰──────────────────────────────────────────╯
+  flake.modules.nixos.greetd =
+    {
+      lib,
+      pkgs,
+      config,
+      ...
+    }:
+    {
+      services.greetd = {
+        enable = true;
+        useTextGreeter = true;
+        settings = {
+          default_session = {
+            command = ''
+              ${lib.getExe pkgs.tuigreet} \
+              --sessions ${config.services.displayManager.sessionData.desktops}/share/wayland-sessions \
+              --time \
+              --time-format '%Y-%m-%d %H:%M' \
+              --asterisks \
+              --remember \
+              --remember-session
+            '';
+          };
+        };
+      };
+    };
+
+  # ╭──────────────────────────────────────────╮
+  # │ LY                                       │
+  # ╰──────────────────────────────────────────╯
+  flake.modules.nixos.ly =
+    { ... }:
+    {
+      services.displayManager.ly = {
+        enable = true;
+        # https://codeberg.org/fairyglade/ly/src/branch/master/res/config.ini
+        settings = {
+          animation = "matrix";
+          bigclock = true;
+          session_log = null;
+          clear_password = true;
+          hide_version_string = true;
+        };
+      };
+    };
+
+  # ╭──────────────────────────────────────────╮
+  # │ DMS                                      │
+  # ╰──────────────────────────────────────────╯
+  flake.modules.nixos.dms =
+    { ... }:
+    {
+      programs.dms-shell = {
+        enable = true;
+      };
+
+      # include fonts:
+      # https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/services/display-managers/dms-greeter.nix#L210
+      services.displayManager.dms-greeter = {
+        # enable = true;
+      };
+
+      # programs.dsearch.enable = true;
     };
 }
