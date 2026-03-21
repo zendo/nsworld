9e391cbebdcb19a57bce245bd5b111b1df7d06e4
zendo <linzway@qq.com>
2026-03-21 18:05:52 +0800

    secrets combine

--- modules/secrets/secrets-module.nix
+++ modules/secrets/secrets-module.nix
@@ -1,3 +1,43 @@
+/*
+  https://github.com/ryantm/agenix
+  https://github.com/oddlama/agenix-rekey
+  https://fzakaria.com/2024/07/12/nix-secrets-for-dummies
+  https://github.com/Mic92/sops-nix
+  https://michael.stapelberg.ch/posts/2025-08-24-secret-management-with-sops-nix/
+
+  mkdir -p ~/.config/sops/age
+
+  - Using host ssh key
+  [private]: ssh-to-age -private-key -i ~/.ssh/id_ed25519 -o ~/.config/sops/age/keys.txt
+  [remote]: sudo ssh-to-age -private-key -i /var/lib/ssh/ssh_host_ed25519_key > ~/.config/sops/age/keys.txt | age-keygen -y
+  chmod 600 ~/.config/sops/age/keys.txt
+
+  - Paste into .sops.yaml
+  ssh-to-age -private-key -i ~/.ssh/id_ed25519 | age-keygen -y
+  cat /var/lib/ssh/ssh_host_ed25519_key.pub | ssh-to-age
+
+  - Edit
+  sops ~/nsworld/modules/secrets/sopsnix.yaml
+
+  - Update all secrets when key changed
+  sops updatekeys modules/secrets/sopsnix.yaml
+
+  - Decrypt and show the real value
+  sops --extract '["dae-sub"]' --decrypt modules/nixos/secrets/sopsnix.yaml
+*/
+let
+  agenixSecrets = {
+    dae.file = ./dae.age;
+    infini_pass.file = ./infini_pass.age;
+  };
+
+  sopsSecrets = {
+    rclone_infini_pass = { };
+    rclone_koofr_pass = { };
+    access_tokens = { };
+    dae_sub = { };
+  };
+in
 {
   flake.modules.nixos.secrets =
     {
@@ -10,16 +50,14 @@
     {
       imports = [
         inputs.agenix.nixosModules.default
-        ./agenix.nix
-
         inputs.sops-nix.nixosModules.sops
-        ./sopsnix.nix
       ];
 
-      # agenix
+      age.secrets = agenixSecrets;
       # age.identityPaths = [ ]; # defined already
 
-      # sops-nix
+      sops.secrets = sopsSecrets;
+      sops.defaultSopsFile = ./sopsnix.yaml;
       # Copy: https://github.com/ryantm/agenix/blob/main/modules/age.nix#L241
       sops.age.sshKeyPaths = map (e: e.path) (
         lib.filter (e: e.type == "rsa" || e.type == "ed25519") config.services.openssh.hostKeys
@@ -46,16 +84,14 @@
     {
       imports = [
         inputs.agenix.homeManagerModules.default
-        ./agenix.nix
-
         inputs.sops-nix.homeManagerModules.sops
-        ./sopsnix.nix
       ];
 
-      # agenix
+      age.secrets = agenixSecrets;
       # age.identityPaths = [ ]; # defined already
 
-      # sops-nix
+      sops.secrets = sopsSecrets;
+      sops.defaultSopsFile = ./sopsnix.yaml;
       sops.age.sshKeyPaths = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
     };
 }
