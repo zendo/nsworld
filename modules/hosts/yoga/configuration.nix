eb588a0951f0649eff1253d45b2b7e260c8d4aff
zendo <linzway@qq.com>
2026-04-02 06:18:21 +0800

    mv

--- modules/hosts/yoga/configuration.nix
+++ modules/hosts/yoga/configuration.nix
@@ -52,52 +52,6 @@
       # nix.package = inputs.determinate.packages.${pkgs.stdenv.hostPlatform.system}.default;
 
       # ╭─────────────────────────────────────────────────────╮
-      # │ PROXY                                               │
-      # ╰─────────────────────────────────────────────────────╯
-      environment.systemPackages = with pkgs; [
-        # inputs.nixpkgs-pr.legacyPackages.${stdenv.hostPlatform.system}.apps
-        inputs.nix-alien.packages.${stdenv.hostPlatform.system}.nix-alien
-        mihomo
-        sing-box
-        # gui-for-singbox
-        # gui-for-clash
-      ];
-
-      systemd.services.sub-store.enable = true;
-
-      services.sing-box = {
-        # enable = true;
-        settings = {
-          _secret = "/home/${myvars.user}/code/subs/config.jsonc";
-          quote = false;
-        };
-      };
-
-      services.mihomo = {
-        enable = true;
-        tunMode = true;
-        # webui = pkgs.zashboard;
-        configFile = "/home/${myvars.user}/code/subs/config.yaml";
-      };
-
-      # services.daeWithConfig.enable = true;
-      # services.dae = {
-      #   enable = true;
-      #   configFile = "/home/${myvars.user}/code/subs/dae-config.dae";
-      # };
-
-      # Virtual network interfering Geoclue2's IP-based location
-      services.geoclue2 = {
-        # enableStatic = true;
-        staticAltitude = 10; # 海拔高度
-        staticAccuracy = 1000; # 精度半径
-      };
-      location = {
-        latitude = 23.38; # 纬度
-        longitude = 113.20; # 经度
-      };
-
-      # ╭─────────────────────────────────────────────────────╮
       # │ HARDWARE                                            │
       # ╰─────────────────────────────────────────────────────╯
       boot = {
---
+++ modules/hosts/yoga/mofa.nix
@@ -0,0 +1,53 @@
+{
+  flake.modules.nixos.host-yoga =
+    {
+      inputs,
+      pkgs,
+      myvars,
+      ...
+    }:
+    {
+      environment.systemPackages = with pkgs; [
+        # inputs.nixpkgs-pr.legacyPackages.${stdenv.hostPlatform.system}.apps
+        inputs.nix-alien.packages.${stdenv.hostPlatform.system}.nix-alien
+        mihomo
+        sing-box
+        # gui-for-singbox
+        # gui-for-clash
+      ];
+
+      systemd.services.sub-store.enable = true;
+
+      services.sing-box = {
+        # enable = true;
+        settings = {
+          _secret = "/home/${myvars.user}/code/subs/config.jsonc";
+          quote = false;
+        };
+      };
+
+      services.mihomo = {
+        enable = true;
+        tunMode = true;
+        # webui = pkgs.zashboard;
+        configFile = "/home/${myvars.user}/code/subs/config.yaml";
+      };
+
+      # services.daeWithConfig.enable = true;
+      # services.dae = {
+      #   enable = true;
+      #   configFile = "/home/${myvars.user}/code/subs/dae-config.dae";
+      # };
+
+      # Virtual network interfering Geoclue2's IP-based location
+      services.geoclue2 = {
+        # enableStatic = true;
+        staticAltitude = 10; # 海拔高度
+        staticAccuracy = 1000; # 精度半径
+      };
+      location = {
+        latitude = 23.38; # 纬度
+        longitude = 113.20; # 经度
+      };
+    };
+}
