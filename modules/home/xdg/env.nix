794b85119263a1b5e57a98518df30699de264f88
zendo <linzway@qq.com>
2026-04-08 10:41:52 +0800

    ...

--- modules/home/xdg/env.nix
+++ modules/home/xdg/env.nix
@@ -10,8 +10,8 @@
 
       home.sessionVariables = {
         # EDITOR = "emacsclient -c -a emacs";
-        EDITOR = "fresh";
-        VISUAL = "fresh";
+        EDITOR = "micro";
+        VISUAL = "micro";
         NIXOS_OZONE_WL = lib.mkDefault 1; # Electron wayland native
         _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
       };
