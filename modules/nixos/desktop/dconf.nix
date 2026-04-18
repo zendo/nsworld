3dc5e2c219b88c68cf928250f8a44c7eca8781de
zendo <linzway@qq.com>
2026-04-18 10:04:50 +0800

    ...

--- modules/nixos/desktop/dconf.nix
+++ modules/nixos/desktop/dconf.nix
@@ -19,6 +19,11 @@
         caffeine
       ];
 
+      # MAYBE show hm .desktop real-time on gnome
+      xdg.systemDirs.data = [
+        "${pkgs.gnome-menus}/share/applications"
+      ];
+
       dconf.settings = {
         "org/gnome/system/location" = {
           enabled = true;
