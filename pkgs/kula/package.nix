70fc5ba6ecabf1cfbfb16a4621c74be6a96b916b
zendo <linzway@qq.com>
2026-04-20 13:34:14 +0800

    up

--- pkgs/kula/package.nix
+++ pkgs/kula/package.nix
@@ -2,32 +2,33 @@
   lib,
   buildGo126Module,
   fetchFromGitHub,
+  writableTmpDirAsHomeHook,
 }:
 
 buildGo126Module (finalAttrs: {
   pname = "kula";
-  version = "0.13.0";
+  version = "0.14.0";
 
   src = fetchFromGitHub {
     owner = "c0m4r";
     repo = "kula";
     tag = finalAttrs.version;
-    hash = "sha256-KzJJYko4oaAQGmxwiFMth8scOuYe0bVB8abe8qal/jE=";
+    hash = "sha256-RSKPMtkoRC06g+doon7ARzsbsJz2fUBHNGl/uPA7l44=";
   };
 
-  vendorHash = "sha256-ZBtlwH54irGAAsh+iLUFs/tnlJWD2GmQi01vSHHVEnw=";
+  vendorHash = "sha256-Bx3CG0ZvMyEu4QJPM+f/AC5wDH2qqo3ByFUbQYB/MAo=";
 
   ldflags = [
     "-s"
     "-w"
   ];
 
-  # Insufficient permissions for /var/lib/kula, falling back to /homeless-shelter/.kula
-  doCheck = false;
+  nativeCheckInputs = [ writableTmpDirAsHomeHook ];
 
   meta = {
     description = "Lightweight, self-contained Linux® server monitoring tool";
-    homepage = "https://github.com/c0m4r/kula";
+    homepage = "https://kula.ovh";
+    downloadPage = "https://github.com/c0m4r/kula";
     changelog = "https://github.com/c0m4r/kula/blob/${finalAttrs.src.rev}/CHANGELOG.md";
     license = lib.licenses.agpl3Only;
     maintainers = with lib.maintainers; [ zendo ];
