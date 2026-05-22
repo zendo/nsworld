ee45a394c54b20c2a8b9a479a9fa6bd313557307
zendo <linzway@qq.com>
2026-05-22 20:59:48 +0800

    open-orpheus init

---
+++ pkgs/open-orpheus/package.nix
@@ -0,0 +1,79 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  dpkg,
+  wrapGAppsHook3,
+  autoPatchelfHook,
+  udev,
+  webkitgtk_4_1,
+  glib-networking,
+  nspr,
+  nss,
+  alsa-lib,
+  libdrm,
+
+  # enableEGL ? true,
+  libglvnd,
+
+  # enableVulkan ? true,
+  vulkan-loader,
+
+  # enableWayland ? true,
+  wayland,
+}:
+let
+  libs = [
+    libglvnd
+    vulkan-loader
+    wayland
+  ];
+in
+stdenv.mkDerivation (finalAttrs: {
+  pname = "open-orpheus";
+  version = "0.12.1";
+
+  src = fetchurl {
+    url = "https://github.com/YUCLing/open-orpheus/releases/download/v${finalAttrs.version}/open-orpheus_${finalAttrs.version}_amd64.deb";
+    hash = "sha256-K7A9DR1NwF4oTbXC1T8x6YQ45v8D6kgKeHs92F0/eXU=";
+  };
+
+  nativeBuildInputs = [
+    dpkg
+    wrapGAppsHook3
+    autoPatchelfHook
+  ];
+
+  buildInputs = [
+    webkitgtk_4_1
+    glib-networking
+    nspr
+    nss
+    alsa-lib
+    libdrm
+    (lib.getLib stdenv.cc.cc)
+  ]
+  ++ libs;
+
+  runtimeDependencies = [ (lib.getLib udev) ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp -r usr/* $out
+  '';
+
+  postFixup = ''
+    wrapProgram $out/bin/open-orpheus \
+      --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath libs}"
+  '';
+
+  meta = {
+    description = "Open-source and cross-platform implementation of Netease Cloud Music's Orpheus browser host";
+    homepage = "https://github.com/YUCLing/open-orpheus";
+    license = lib.licenses.mit;
+    platforms = [ "x86_64-linux" ];
+    sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
+    maintainers = with lib.maintainers; [ zendo ];
+    mainProgram = "open-orpheus";
+  };
+})
