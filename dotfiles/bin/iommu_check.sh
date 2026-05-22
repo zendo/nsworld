262ca44574a599fb634f716878f3397fdc687a37
zendo <linzway@qq.com>
2026-05-22 11:55:42 +0800

    fix

--- dotfiles/bin/iommu_check.sh
+++ dotfiles/bin/iommu_check.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 shopt -s nullglob
-for g in $(find /sys/kernel/iommu_groups/* -maxdepth 0 -type d | sort -V); do
-  echo "IOMMU Group ${g##*/}:"
-  for d in $g/devices/*; do
-    echo -e "\t$(lspci -nns ${d##*/})"
-  done
+for g in $(printf '%s\n' /sys/kernel/iommu_groups/* | sort -V); do
+    [[ -d "$g" ]] || continue
+
+    echo "IOMMU Group ${g##*/}:"
+    for d in "$g"/devices/*; do
+        echo -e "\t$(lspci -nn -s "${d##*/}")"
+    done
 done
