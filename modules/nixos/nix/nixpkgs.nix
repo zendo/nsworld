8829ce47667fcd4e740394fa08d497da5110f3df
zendo <linzway@qq.com>
2026-07-04 11:46:44 +0800

    ...

--- modules/nixos/nix/nixpkgs.nix
+++ modules/nixos/nix/nixpkgs.nix
@@ -15,26 +15,28 @@
           allowUnfreePredicate =
             pkg:
             builtins.elem (lib.getName pkg) [
-              "claude-code"
-              "amp-cli"
               "unrar"
               "spotify"
               "qqmusic"
+              "keyguard"
               "vivaldi"
               "google-chrome"
               "microsoft-edge"
               "widevine-cdm"
-              "keyguard"
+              "memtest86-efi"
+              # ------
+              "android-sdk-cmdline-tools"
+              "amp-cli"
+              "claude-code"
+              "vscode"
+              "vscode-extension-ms-vscode-cpptools"
+              "vscode-extension-ms-vscode-remote-remote-ssh"
+              # ------
               "steam"
               "steam-unwrapped"
               "balatro"
               "pvzge"
               "pvz_logo.webp"
-              "memtest86-efi"
-              "android-sdk-cmdline-tools"
-              "vscode"
-              "vscode-extension-ms-vscode-cpptools"
-              "vscode-extension-ms-vscode-remote-remote-ssh"
             ];
         };
       };
