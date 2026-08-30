79178eed6128942cbd6f437349d58a3a7de28bc5
zendo <linzway@qq.com>
2026-08-31 03:51:08 +0800

    clean

--- flake.nix
+++ flake.nix
@@ -74,10 +74,5 @@
   # Flake Local Nix Configuration
   nixConfig = {
     # bash-prompt = "[nix]λ ";
-    # substituters = [ "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store" ];
-    # extra-substituters = [ "https://nix-community.cachix.org" ];
-    # extra-trusted-public-keys = [
-    #   "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    # ];
   };
 }
