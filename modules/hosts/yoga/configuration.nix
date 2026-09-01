b05ef7cc7e51f309eb590f3196f16ab73186f648
zendo <linzway@qq.com>
2026-09-02 00:13:42 +0800

    add

--- modules/hosts/yoga/configuration.nix
+++ modules/hosts/yoga/configuration.nix
@@ -51,6 +51,7 @@ in
       # systemd.services.comigo.enable = true;
 
       environment.systemPackages = with pkgs; [
+        inputs.omniflake.flakes.nix-packages-ccicnce113424.packages.x86_64-linux.open-orpheus
         # [ deploy ]
         disko
         nixos-anywhere
