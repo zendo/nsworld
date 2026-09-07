3dea8980b88d7aef737348421be3ef25adec4ff4
zendo <linzway@qq.com>
2026-09-07 22:48:08 +0800

    fixup! firefox: nixos module init

--- modules/nixos/programs/firefox.nix
+++ modules/nixos/programs/firefox.nix
@@ -9,20 +9,28 @@
 
         preferencesStatus = "default";
         preferences = {
+          # Languages
           "intl.accept_languages" = "zh-CN, en";
           "intl.locale.requested" = "zh-CN";
-          "sidebar.verticalTabs" = true;
+
+          # Prompts
           "browser.aboutwelcome.enabled" = false;
           "browser.aboutConfig.showWarning" = false;
-          "browser.translations.automaticallyPopup" = false; # Don't ask to translate
           "signon.rememberSignons" = false; # Don't ask to save passwords
+          "browser.translations.automaticallyPopup" = false; # Don't ask to translate
+
+          # Bookmarks
           "browser.toolbars.bookmarks.visibility" = "always";
+
+          # Tabs
+          "sidebar.verticalTabs" = true;
           "browser.tabs.closeTabByDblclick" = true;
           "browser.tabs.closeWindowWithLastTab" = false; # Prevent closing Firefox when closing the last tab
           "browser.tabs.selectOwnerOnClose" = false; # Closing tab and return to the adjacent tab
           "browser.tabs.insertAfterCurrent" = true; # Open new tabs next to current
           "browser.tabs.loadBookmarksInTabs" = true; # Open bookmarks in a new tab
-          # https://pandasauce.org/get-fonts-done/
+
+          # Fonts: https://pandasauce.org/get-fonts-done/
           "gfx.text.subpixel-position.force-enabled" = true;
           "gfx.webrender.quality.force-subpixel-aa-where-possible" = true;
         };
