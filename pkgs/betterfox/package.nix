0bd22905a6dec490db01a2460939a9df5f85fbd1
zendo <linzway@qq.com>
2026-09-07 23:32:54 +0800

    betterfox: 152.0 -> 154.0

--- pkgs/betterfox/package.nix
+++ pkgs/betterfox/package.nix
@@ -3,6 +3,6 @@
 fetchFromGitHub {
   owner = "yokoffing";
   repo = "Betterfox";
-  tag = "152.0";
-  hash = "sha256-QfisBzeoc8nUrgk/+PTvG9JATsVRWo7RDbohlxkNc+A=";
+  tag = "154.0";
+  hash = "sha256-mIP/WcXUcGrJsWCJzR4zqPOmt0BbbpTZVaN/MbIwBbw=";
 }
