8989ede46c1162bfa55133959619b5e7f32a884c
zendo <linzway@qq.com>
2026-09-07 23:30:44 +0800

    firefox: fix

--- modules/home/programs/firefox.nix
+++ modules/home/programs/firefox.nix
@@ -11,16 +11,28 @@
         profiles.huohu = {
           isDefault = true;
           settings = {
+            # Languages
+            "intl.accept_languages" = "zh-CN, en";
+            "intl.locale.requested" = "zh-CN";
+
+            # Prompts
+            "browser.aboutwelcome.enabled" = false;
+            "browser.aboutConfig.showWarning" = false;
+            "signon.rememberSignons" = false; # Don't ask to save passwords
+            "browser.translations.automaticallyPopup" = false; # Don't ask to translate
+
+            # Bookmarks
+            "browser.toolbars.bookmarks.visibility" = "always";
+
+            # Tabs
+            "sidebar.verticalTabs" = true;
             "browser.tabs.closeTabByDblclick" = true;
             "browser.tabs.closeWindowWithLastTab" = false; # Prevent closing Firefox when closing the last tab
             "browser.tabs.selectOwnerOnClose" = false; # Closing tab and return to the adjacent tab
             "browser.tabs.insertAfterCurrent" = true; # Open new tabs next to current
             "browser.tabs.loadBookmarksInTabs" = true; # Open bookmarks in a new tab
-            "browser.toolbars.bookmarks.visibility" = "always";
-            "browser.aboutwelcome.enabled" = false;
-            "browser.aboutConfig.showWarning" = false;
 
-            # https://pandasauce.org/get-fonts-done/
+            # Fonts: https://pandasauce.org/get-fonts-done/
             "gfx.text.subpixel-position.force-enabled" = true;
             "gfx.webrender.quality.force-subpixel-aa-where-possible" = true;
           };
