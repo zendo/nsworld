8f3abfbb44a689977c044ab492dbf1f5b6f2bf22
zendo <linzway@qq.com>
2026-09-13 11:03:04 +0800

    clean

--- modules/flake/wrapper/git.nix
+++ modules/flake/wrapper/git.nix
@@ -1,4 +1,5 @@
 # WIP
+# git config --global --show-origin --list
 { inputs, ... }:
 let
   gitName = "zendo";
@@ -15,6 +16,39 @@ in
             delta
             git-credential-oauth
           ];
+          settings = {
+            user.name = gitName;
+            user.email = gitEmail;
+            column.ui = "auto";
+            init.defaultBranch = "main";
+            # pull.rebase = true;
+            # merge.ff = "only";
+
+            core.excludesfile = config.constructFiles.ignore.path;
+            core.pager = "${lib.getExe pkgs.delta}";
+            interactive.diffFilter = "${lib.getExe pkgs.delta} --color-only";
+            credential.helper = "${lib.getExe pkgs.git-credential-oauth}";
+
+            # safe.directory = [
+            #   "~/nsworld"
+            #   "~/Projects/nixpkgs"
+            # ];
+
+            alias = {
+              st = "status -sb";
+              ds = "diff --stat";
+              unstage = "reset HEAD --";
+              undo = "reset --hard HEAD~1";
+              patch = "format-patch --stdout HEAD~1";
+              rpatch = "reset --hard HEAD~1";
+              rank = "shortlog -s -n --no-merges";
+              ls = "log --stat --abbrev-commit";
+              lg = "log --graph --decorate --all --oneline";
+              ll = "log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr %an)%Creset' --abbrev-commit --date=relative";
+              pr-pull = "!f() { git fetch upstream pull/$1/head:pr-$2-$1 && git checkout pr-$2-$1; }; f";
+            };
+          }; # settings
+
           constructFiles.ignore = {
             relPath = "ignore";
             content = ''
@@ -34,37 +68,7 @@ in
               .venv/
             '';
           };
-          settings = {
-            user.name = gitName;
-            user.email = gitEmail;
-            init.defaultBranch = "main";
-            column.ui = "auto";
-            core.excludesfile = config.constructFiles.ignore.path;
-            core.pager = "${lib.getExe pkgs.delta}";
-            interactive.diffFilter = "${lib.getExe pkgs.delta} --color-only";
 
-            credential.helper = "${lib.getExe pkgs.git-credential-oauth}";
-            # pull.rebase = true;
-            # merge.ff = "only";
-            # core.editor = "vim";
-            # safe.directory = [
-            #   "${config.home.homeDirectory}/nsworld"
-            #   "${config.home.homeDirectory}/Projects/nixpkgs"
-            # ];
-            alias = {
-              st = "status -sb";
-              ds = "diff --stat";
-              unstage = "reset HEAD --";
-              undo = "reset --hard HEAD~1";
-              patch = "format-patch --stdout HEAD~1";
-              rpatch = "reset --hard HEAD~1";
-              rank = "shortlog -s -n --no-merges";
-              ls = "log --stat --abbrev-commit";
-              lg = "log --graph --decorate --all --oneline";
-              ll = "log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr %an)%Creset' --abbrev-commit --date=relative";
-              pr-pull = "!f() { git fetch upstream pull/$1/head:pr-$2-$1 && git checkout pr-$2-$1; }; f";
-            };
-          };
         }
       );
     };
