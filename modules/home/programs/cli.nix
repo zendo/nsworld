2ac2914db406fd2789c853116701dc45aae0bd71
zendo <linzway@qq.com>
2026-09-13 14:27:02 +0800

    ...

--- modules/home/programs/cli.nix
+++ modules/home/programs/cli.nix
@@ -7,7 +7,6 @@
       programs = {
         man.enable = false; # man-db
         zoxide.enable = true; # z: autojump directory
-        # pay-respects.enable = true;
       };
 
       programs.fzf = {
