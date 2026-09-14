1de791ef292b8968e4fc5b7e37773c8318edaabc
zendo <linzway@qq.com>
2026-09-14 15:02:53 +0800

    zsh: move to nixos module

--- modules/home/shell/zsh.nix
+++ modules/home/shell/zsh.nix
@@ -1,41 +1,75 @@
 {
-  flake.modules.nixos.zsh = {
-    programs.zsh.enable = true;
+  flake.modules.nixos.zsh = { pkgs, ... }: {
+    programs.zsh = {
+      enable = true;
+      autosuggestions.enable = true;
+      # syntaxHighlighting.enable = true;
+      promptInit = ''
+        fpath+=("${pkgs.pure-prompt}/share/zsh/site-functions")
+        autoload -U promptinit && promptinit
+        prompt pure
+      '';
+      interactiveShellInit = ''
+        unsetopt correct  # Disable AutoCorrect
+
+        # Completions
+        zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' # Case insensitive
+        zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}" # Colored
+        zstyle ':completion:*' menu select                        # Hit 'TAB' to select
+
+        # Bash-like navigation between words
+        autoload -U select-word-style
+        select-word-style bash
+
+        # Keybindings
+        bindkey -e                                        # Emacs keybinding
+        bindkey "^[[3~" delete-char                       # Del key
+        bindkey "^[[H" beginning-of-line                  # Home key
+        bindkey "^[[F" end-of-line                        # End key
+        bindkey "^[[5~" history-beginning-search-backward # Page up key
+        bindkey "^[[6~" history-beginning-search-forward  # Page down key
+        bindkey "\e[27;2;13~" accept-line                 # Shift - enter
+        bindkey "\e[27;5;13~" accept-line                 # Ctrl  - enter
+        bindkey "^H" backward-kill-word                   # Ctrl  - backspace
+        bindkey "^[[1;5C" forward-word                    # Ctrl  - →
+        bindkey "^[[1;5D" backward-word                   # Ctrl  - ←
+        bindkey "^[[1;3C" forward-word                    # Alt   - →
+        bindkey "^[[1;3D" backward-word                   # Alt   - ←
+        bindkey '\e[1;3A' beginning-of-line               # ALT   - ↑
+        bindkey '\e[1;3B' end-of-line                     # ALT   - ↓
+      '';
+    };
+
+    environment.etc."zshrc.local".text = ''
+      source ${pkgs.zsh-fast-syntax-highlighting}/share/zsh/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+      # eval "$(${pkgs.zsh-patina}/bin/zsh-patina activate)"
+    '';
   };
 
   flake.modules.homeManager.zsh =
     {
       config,
       pkgs,
-      lib,
-      osConfig ? null,
       ...
     }:
-    lib.mkIf (osConfig == null || osConfig.programs.zsh.enable) {
-
-      # home.packages = with pkgs; [
-      #   pure-prompt
-      # ];
-
+    {
       programs.zsh = {
         enable = true;
         enableCompletion = true;
         autosuggestion.enable = true;
-        fastSyntaxHighlighting.enable = true; # or zsh-patina
+        fastSyntaxHighlighting.enable = true;
         history = {
           path = "${config.xdg.dataHome}/zsh/history";
           extended = true; # timestamp
           expireDuplicatesFirst = true;
         };
-        shellAliases = {
-          history = "history 0"; # show whole history
-        };
         initContent = ''
           unsetopt correct  # Disable AutoCorrect
 
           # Theme
-          # autoload -U promptinit; promptinit
-          # prompt pure
+          fpath+=("${pkgs.pure-prompt}/share/zsh/site-functions")
+          autoload -U promptinit && promptinit
+          prompt pure
 
           # Completions
           zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' # Case insensitive
