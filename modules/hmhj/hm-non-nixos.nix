adecd8c174ebfa173968e6ccde420004f68057a4
zendo <linzway@qq.com>
2026-09-17 08:08:46 +0800

    non-nixos: clean

--- modules/hmhj/hm-non-nixos.nix
+++ modules/hmhj/hm-non-nixos.nix
@@ -8,15 +8,16 @@
       ];
 
       news.display = "silent";
-      programs.home-manager.enable = true;
       manual.manpages.enable = false; # HM manuals
       programs.man.enable = false; # man-db
+      programs.home-manager.enable = true;
+
       # https://github.com/nix-community/home-manager/blob/master/docs/manual/usage/gpu-non-nixos.md
       targets.genericLinux.gpu.enable = true;
 
-      # ========================================================
+      # ===================================================
       #  Nix Settings
-      # ========================================================
+      # ===================================================
       nix = {
         package = pkgs.nixVersions.stable;
         registry = {
@@ -40,30 +41,5 @@
           ];
         }; # end of settings
       }; # end of nix
-
-      # ========================================================
-      #  Fonts Config
-      # ========================================================
-      fonts.fontconfig = {
-        enable = true;
-        defaultFonts = {
-          emoji = [ "Noto Color Emoji" ];
-          monospace = [
-            "Noto Sans Mono CJK SC"
-            "Sarasa Mono SC"
-            "DejaVu Sans Mono"
-          ];
-          sansSerif = [
-            "Noto Sans CJK SC"
-            "Source Han Sans SC"
-            "DejaVu Sans"
-          ];
-          serif = [
-            "Noto Serif CJK SC"
-            "Source Han Serif SC"
-            "DejaVu Serif"
-          ];
-        }; # end of defaultFonts
-      }; # end of fontconfig
     };
 }
