d6ae9c434a2233badf31026c254c9c28510dc397
zendo <linzway@qq.com>
2026-09-19 07:43:31 +0800

    flyline: 1.7.1 -> 1.8.0
    Diff: https://github.com/HalFrgrd/flyline/compare/v1.7.1...v1.8.0
    
    Changelog: https://github.com/HalFrgrd/flyline/releases/tag/v1.8.0

--- pkgs/flyline/package.nix
+++ pkgs/flyline/package.nix
@@ -6,17 +6,17 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "flyline";
-  version = "1.7.1";
+  version = "1.8.0";
   __structuredAttrs = true;
 
   src = fetchFromGitHub {
     owner = "HalFrgrd";
     repo = "flyline";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-MscTAE921ymn0qyL3YQ06P3+AGg23c//6rh7+c+Iv4Y=";
+    hash = "sha256-gmkp9gxI5xcU0n2rCc1TBh7C+cWi0GwIUyP7C+xKuA4=";
   };
 
-  cargoHash = "sha256-xbUyrhiBt8XQh2+rcOg4tnXAYmiAbtWbQH4462D0fGI=";
+  cargoHash = "sha256-pf8JMBQxUoLjo7mpqFYa/yJpZc2ZJOZqMx0oMEKB7wA=";
 
   doCheck = false;
 
