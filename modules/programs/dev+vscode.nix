9a3dda2a9ec0d141001a0d17a777cd7dd5f6ab7d
zendo <linzway@qq.com>
2026-09-22 17:01:10 +0800

    del vscode.nix

--- modules/programs/dev+vscode.nix
+++ modules/programs/dev+vscode.nix
@@ -1,4 +1,37 @@
 {
+  flake.modules.nixos.dev =
+    { pkgs, ... }:
+    {
+      environment.systemPackages = with pkgs; [
+        # [ ide ]
+        # zed-editor
+
+        # [ base ]
+        gcc
+        cmake
+        gnumake
+
+        # [ python ]
+        python3
+        pylyzer
+        # ruff
+        # uv
+
+        # [ forntend ]
+        # nodejs
+
+        # [ lsp ]
+        bash-language-server
+        typescript-language-server
+        lua-language-server
+        efm-langserver
+        # nil
+        nixd
+        # devenv
+        flamegraph
+      ];
+    };
+
   flake.modules.homeManager.vscode =
     { pkgs, ... }:
     {
